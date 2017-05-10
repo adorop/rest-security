dcb71a07d9bae23b69432647a697309cbc070a3e
Aleksey Darapiyevich <aleksey.darapiyevich@draytus.com>
2017-05-10 19:30:14 +0300

    configuring default content type

--- src/main/java/adorop/web/WebConfig.java
+++ src/main/java/adorop/web/WebConfig.java
@@ -5,8 +5,11 @@ import adorop.security.SecurityConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,5 +20,8 @@ import java.util.List;
 @Import({PersistenceConfig.class, SecurityConfig.class})
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
-
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
 }
